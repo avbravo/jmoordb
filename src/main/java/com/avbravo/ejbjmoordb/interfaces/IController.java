4a22daee3a59e964d6843e1b8db9967c52c51d7f
avbravo <avbravo@avbravo>
2017-12-28 16:23:21 -0500

    0.3.3 incorpora la interface IController

---
+++ src/main/java/com/avbravo/ejbjmoordb/interfaces/IController.java
@@ -0,0 +1,68 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avbravo.ejbjmoordb.interfaces;
+
+
+
+/**
+ *
+ * @authoravbravo
+ */
+public interface IController<T> {
+
+//action= new, search, writable, ("edit", entity)
+    public String prepare(String action, Object... item) ;
+    
+//public String prepareNew();
+//    //public String open(); se reemplazo por prepareSearch
+//    public String prepareSearch();
+//    //public String query();
+//
+//    public String prepareWritable();
+
+    
+    
+
+    public String isNew();
+
+    public void reset();
+
+    public String showAll();
+
+    public String save();
+
+    public String edit();
+
+  //  public String prepareEdit(Object item);
+
+    /*
+      donotleave= true se queda en el form false = salta al formulario list
+      reset = true resetea el formulario (view), list reset = false;
+      removeFromList= true remueve de la lista se llama desde el list.
+     */
+    public String delete(Object item, Boolean donotleave, Boolean reset, Boolean removeFromList);
+
+    public String deleteAll();
+
+    public String print();
+
+    public String printAll();
+
+ //public void handleSelect(SelectEvent event);
+
+    public String last();
+
+    public String first();
+
+    public String next();
+
+    public String back();
+
+    public String skip(Integer page);
+
+    public void move();
+
+}
