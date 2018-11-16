a6554161472af7794845b17cfc1132278cb9149f
avbravo <avbravo@avbravo>
2018-11-16 11:16:21 -0500

    0.8.10

--- src/main/java/com/avbravo/ejbjmoordb/pojos/ErrorInfo.java
+++ src/main/java/com/avbravo/ejbjmoordb/pojos/ErrorInfo.java
@@ -18,17 +18,38 @@ public class ErrorInfo {
     private String username;
     private Date date;
     private String clase;
+    private String metodo;
     private String mensaje;
 
     public ErrorInfo() {
     }
 
+    public ErrorInfo(String iderror, String username, Date date, String clase, String metodo, String mensaje) {
+        this.iderror = iderror;
+        this.username = username;
+        this.date = date;
+        this.clase = clase;
+        this.metodo = metodo;
+        this.mensaje = mensaje;
+    }
+
+    
+    
+    
     public String getIderror() {
         return iderror;
     }
 
     public void setIderror(String iderror) {
         this.iderror = iderror;
+    }
+
+    public String getMetodo() {
+        return metodo;
+    }
+
+    public void setMetodo(String metodo) {
+        this.metodo = metodo;
     }
 
   
