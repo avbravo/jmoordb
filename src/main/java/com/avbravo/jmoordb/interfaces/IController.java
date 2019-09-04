234ca37122fc3a8985a3afa2ce4d860a657b5c3e
avbravo <avbravo@avbravo>
2019-09-04 13:44:17 -0500

    0.42

--- src/main/java/com/avbravo/jmoordb/interfaces/IController.java
+++ src/main/java/com/avbravo/jmoordb/interfaces/IController.java
@@ -2340,5 +2340,130 @@ public interface IController<T> {
     } // </editor-fold>
     
     
+// <editor-fold defaultstate="collapsed" desc="Object getAction() )">
 
+    /**
+     * Obtiene el valor del action 
+     *
+     * @return
+     */
+    default String getAction() {
+        String actionLocal= "golist";
+        try {
+            JmoordbConfiguration jmc = new JmoordbConfiguration();
+            JmoordbControllerEnvironment jme = new JmoordbControllerEnvironment();
+            String username = jmc.getUsername();
+            Repository repositoryRevisionHistory = jmc.getRepositoryRevisionHistory();
+            RevisionHistoryServices revisionHistoryServices = jmc.getRevisionHistoryServices();
+            Boolean saverevision = jmc.getRevisionSave();
+            Boolean languaguespanish = jmc.getSpanish();
+
+            Boolean spanish = true;
+            if (languaguespanish == null) {
+                JmoordbUtil.warningMessage("Configure el parametro {languaguespanish} en el ExternalContext de la clase principal");
+            } else {
+                spanish = languaguespanish;
+            }
+
+            if (saverevision == null) {
+                JmoordbUtil.warningMessage(spanish ? "Configure el parametro {saverevision} en el ExternalContext de la clase principal" : "Configure the {saverevision} parameter in the ExternalContext of the main class");
+                saverevision = false;
+            }
+
+            //Obtenerlos desde el JmoordbControllerEnvironment
+            Repository repository = jme.getRepository();
+            Object controller = jme.getController();
+            Object entity = jme.getEntity();
+            Object service = jme.getService();
+            String nameFieldOfPage = jme.getNameFieldOfPage();
+            String nameFieldOfRowPage = jme.getNameFieldOfRowPage();
+            String typeKey = jme.getTypeKey();
+            Boolean searchLowerCase = jme.getSearchLowerCase();
+            Boolean resetInSave = jme.getResetInSave();
+            String pathReportDetail = jme.getPathReportDetail();
+            String pathReportAll = jme.getPathReportAll();
+            String action = jme.getAction();
+            String nameOfController = controller.getClass().getSimpleName();
+            HashMap parameters = jme.getParameters();
+            String nameOfEntity = JmoordbIntrospection.nameOfEntity(entity);
+            entity = (Object) JmoordbIntrospection.callGet(controller, nameOfEntity);
+
+            if (JmoordbContext.get("action" + nameOfController) == null) {
+                JmoordbContext.put("action" + nameOfController, "golist");
+            }
+           actionLocal =(String) JmoordbContext.get("action" + nameOfController);
+
+        } catch (Exception ex) {
+
+            JmoordbUtil.errorMessage(nameOfMethod() + " " + ex.getLocalizedMessage());
+
+        }
+
+        return actionLocal;
+
+    } // </editor-fold>
+// <editor-fold defaultstate="collapsed" desc="Object getAction() )">
+
+    /**
+     * Asigna valor al action
+     *
+     * @return
+     */
+    default void setAction(String actionValue) {
+       
+        try {
+            JmoordbConfiguration jmc = new JmoordbConfiguration();
+            JmoordbControllerEnvironment jme = new JmoordbControllerEnvironment();
+            String username = jmc.getUsername();
+            Repository repositoryRevisionHistory = jmc.getRepositoryRevisionHistory();
+            RevisionHistoryServices revisionHistoryServices = jmc.getRevisionHistoryServices();
+            Boolean saverevision = jmc.getRevisionSave();
+            Boolean languaguespanish = jmc.getSpanish();
+
+            Boolean spanish = true;
+            if (languaguespanish == null) {
+                JmoordbUtil.warningMessage("Configure el parametro {languaguespanish} en el ExternalContext de la clase principal");
+            } else {
+                spanish = languaguespanish;
+            }
+
+            if (saverevision == null) {
+                JmoordbUtil.warningMessage(spanish ? "Configure el parametro {saverevision} en el ExternalContext de la clase principal" : "Configure the {saverevision} parameter in the ExternalContext of the main class");
+                saverevision = false;
+            }
+
+            //Obtenerlos desde el JmoordbControllerEnvironment
+            Repository repository = jme.getRepository();
+            Object controller = jme.getController();
+            Object entity = jme.getEntity();
+            Object service = jme.getService();
+            String nameFieldOfPage = jme.getNameFieldOfPage();
+            String nameFieldOfRowPage = jme.getNameFieldOfRowPage();
+            String typeKey = jme.getTypeKey();
+            Boolean searchLowerCase = jme.getSearchLowerCase();
+            Boolean resetInSave = jme.getResetInSave();
+            String pathReportDetail = jme.getPathReportDetail();
+            String pathReportAll = jme.getPathReportAll();
+            String action = jme.getAction();
+            String nameOfController = controller.getClass().getSimpleName();
+            HashMap parameters = jme.getParameters();
+            String nameOfEntity = JmoordbIntrospection.nameOfEntity(entity);
+            entity = (Object) JmoordbIntrospection.callGet(controller, nameOfEntity);
+
+            
+                JmoordbContext.put("action" + nameOfController, actionValue);
+            jme.setAction(actionValue);
+            
+     
+
+        } catch (Exception ex) {
+
+            JmoordbUtil.errorMessage(nameOfMethod() + " " + ex.getLocalizedMessage());
+
+        }
+
+  
+
+    } // </editor-fold>
+    
 }
