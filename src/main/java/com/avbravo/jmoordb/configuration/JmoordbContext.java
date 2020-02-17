9f554324499522561c37f714dd3653145d6f950a
avbravo <avbravo@gmail.com>
2020-02-17 10:49:04 -0500

    0.53 putApplication, getApplication en JmoordbContext

--- src/main/java/com/avbravo/jmoordb/configuration/JmoordbContext.java
+++ src/main/java/com/avbravo/jmoordb/configuration/JmoordbContext.java
@@ -14,6 +14,12 @@ import javax.faces.context.FacesContext;
  */
 public class JmoordbContext {
 
+    // <editor-fold defaultstate="collapsed" desc="void put(String key, Object value)">
+    /**
+     * guarda en el session
+     * @param key
+     * @param value 
+     */
     public static void put(String key, Object value) {
         try{
              FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put(key, value);
@@ -23,13 +29,45 @@ public class JmoordbContext {
       
        
     }
-    public static Object get(String key) {
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="putApplication()">
+    public static void putApplication(String key, Object value) {
         try{
-             return FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(key);
+             FacesContext.getCurrentInstance().getExternalContext().getApplicationMap().put(key, value);
         }catch(Exception e){
               JmoordbUtil.errorMessage("put()" + " " + e.getLocalizedMessage());
         }  
       
+       
+    }
+    
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Object get(String key)">
+    public static Object get(String key) {
+        try{
+             return FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(key);
+        }catch(Exception e){
+              JmoordbUtil.errorMessage("get()" + " " + e.getLocalizedMessage());
+        }  
+      
        return "";
     }
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Object getApplication(String key)">
+    /**
+     * GetApplication obtiene el valor del application
+     * @param key
+     * @return 
+     */
+    public static Object getApplication(String key) {
+        try{
+             return FacesContext.getCurrentInstance().getExternalContext().getApplicationMap().get(key);
+        }catch(Exception e){
+              JmoordbUtil.errorMessage("getApplication()" + " " + e.getLocalizedMessage());
+        }  
+      
+       return "";
+    }
+    // </editor-fold>
 }
